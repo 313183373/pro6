638438acf16da0988b1420abe0fc0d4e33cdd15f
star xiong <313183373@qq.com>
2017-05-28 20:56:35 +0800

    1

--- main/main.js
+++ main/main.js
@@ -1,5 +1,5 @@
 module.exports = function main(email, suffixes) {
-	var houzhui=email.split('@')[1].split('.',1);
+	var houzhui=email.split('@')[1];
 	if(suffixes.indexOf(houzhui)>-1)
 		return true;
 	return false;
